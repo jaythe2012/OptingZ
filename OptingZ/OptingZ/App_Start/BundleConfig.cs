fc764e1717e79142fac543babcf7c8259ca9f1f2
Vishal <Vishal@VISHAL-PC>
2016-05-04 07:31:16 -0400

    Just adding these changes to my dev brach to snyc bet laptops

--- OptingZ/OptingZ/App_Start/BundleConfig.cs
+++ OptingZ/OptingZ/App_Start/BundleConfig.cs
@@ -11,16 +11,11 @@ namespace OptingZ
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            "~/Scripts/jquery-ui-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
@@ -43,6 +38,12 @@ namespace OptingZ
               "~/Content/themes/base/jquery.ui.datepicker.css",
               "~/Content/themes/base/jquery.ui.progressbar.css",
               "~/Content/themes/base/jquery.ui.theme.css"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
         }
     }
 }
