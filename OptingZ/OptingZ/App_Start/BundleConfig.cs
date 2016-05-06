1513e072e7d4ee603b9a4ab3ce6a17f15012019a
Vishal <Vishal@VISHAL-PC>
2016-05-06 06:56:14 -0400

    Jquery ui file issue fixed

--- OptingZ/OptingZ/App_Start/BundleConfig.cs
+++ OptingZ/OptingZ/App_Start/BundleConfig.cs
@@ -21,23 +21,18 @@ namespace OptingZ
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new StyleBundle("~/Content/themes/base").Include(
+              "~/Content/themes/base/all.css",
+              "~/Content/themes/base/core.css",
+              "~/Content/themes/base/autocomplete.css",
+              "~/Content/themes/base/jquery.ui.button.css",
+              "~/Content/themes/base/jquery.ui.datepicker.css",
+              "~/Content/themes/base/jquery.ui.theme.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/jquery-ui-1.8.21.custom.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-              "~/Content/themes/base/jquery.ui.core.css",
-              "~/Content/themes/base/jquery.ui.resizable.css",
-              "~/Content/themes/base/jquery.ui.selectable.css",
-              "~/Content/themes/base/jquery.ui.accordion.css",
-              "~/Content/themes/base/jquery.ui.autocomplete.css",
-              "~/Content/themes/base/jquery.ui.button.css",
-              "~/Content/themes/base/jquery.ui.dialog.css",
-              "~/Content/themes/base/jquery.ui.slider.css",
-              "~/Content/themes/base/jquery.ui.tabs.css",
-              "~/Content/themes/base/jquery.ui.datepicker.css",
-              "~/Content/themes/base/jquery.ui.progressbar.css",
-              "~/Content/themes/base/jquery.ui.theme.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
