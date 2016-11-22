07459eeac32c5aa4b6f5ecb4b0dd63834428d44b
jaythe20 <jaythe20@gmail.com>
2016-11-22 15:17:38 -0500

    First commit from Rider JetBrains

--- OptingZ/OptingZ/Controllers/CategoryMastersController.cs
+++ OptingZ/OptingZ/Controllers/CategoryMastersController.cs
@@ -43,6 +43,7 @@ namespace OptingZ.Controllers
         }
 
         // POST: CategoryMasters/Create
+        // Trying to commit from JetBrains
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
